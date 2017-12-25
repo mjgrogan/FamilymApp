e1c6b92511f69e4c075aeb176018fa1c484efda9
mjgrogan <mjgrogan@outlook.com>
2017-12-24 17:44:52 -0800

    Completed Sign In data validation.

--- app/src/main/java/quitessence/www/familymapp/SignIn.java
+++ app/src/main/java/quitessence/www/familymapp/SignIn.java
@@ -1,20 +1,34 @@
 package quitessence.www.familymapp;
 
 import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 public class SignIn extends AppCompatActivity {
-    //public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    private TextInputLayout Username_til, Password_til;
+    private EditText Username_et, Password_et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        initializeWidgets();
     }
+
+    private void initializeWidgets() {
+        Username_til = (TextInputLayout) findViewById(R.id.tilUsername);
+        Username_et = (EditText) findViewById(R.id.etUsername);
+        Password_til = (TextInputLayout) findViewById(R.id.tilPassword);
+        Password_et = (EditText) findViewById(R.id.etPassword);
+    }
+
     /** Called when the user taps the "New to Family mApp?. Sign up now? */
     public void Register_mthd(View view) {
          Intent intent = new Intent(this, Register.class);
@@ -26,8 +40,40 @@ public class SignIn extends AppCompatActivity {
 
     /** Called when the user taps the "Sign In" button */
     public void SignIn_mthd(View view) {
+
+        /** Used to confirm SignIn_mthd call / Username and Password validation code
+        Toast toastvalidation = Toast.makeText(getApplicationContext(), "Test Username and Password validation code", Toast.LENGTH_SHORT);
+        toastvalidation.show();
+        */
+
+        boolean isUsernameValid = true;
+        if(Username_et.getText().toString().trim().length() < 6) {
+            Username_til.setError("Username is too short (must be a minimum of 6 characters). Please try again...");
+            isUsernameValid = false;
+        } else {
+            Username_til.setErrorEnabled(false);
+        }
+        boolean isPasswordValid = true;
+        if(Password_et.getText().toString().trim().length() < 6) {
+            Password_til.setError("Password is too short (must be a minimum of 6 characters). Please try again...");
+            isPasswordValid = false;
+        } else {
+            Password_til.setErrorEnabled(false);
+        }
+
+        if(isUsernameValid && isPasswordValid) {
+            Toast toastokay = Toast.makeText(getApplicationContext(), "Both are okay", Toast.LENGTH_SHORT);
+            toastokay.show();
+        }
+
+
         Toast toast = Toast.makeText(getApplicationContext(), "Test PHP Sign In Script", Toast.LENGTH_SHORT);
         toast.show();
+
+
+
+
+
     }
 
     /** Called when the user taps the "Go To Main Activity" button */
